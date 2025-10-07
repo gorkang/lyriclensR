120f3969949a3a2e6b55ef8b2e358e94d2fede82
G <gorkang@gmail.com>
2025-10-07 11:32:09 +0100

    Add spotify related folders

--- R/make_CS.R
+++ R/make_CS.R
@@ -7,6 +7,10 @@ data.table::fwrite(DF_lyrics_paragraphs_HITS$DF_lyrics_HITS,paste0(destination_f
 data.table::fwrite(DF_lyrics_paragraphs_HITS$DF_paragraphs_HITS, paste0(destination_folder, "DF_paragraphs_HITS.gz"))
 data.table::fwrite(DF_lyrics_updated, paste0(destination_folder, "DF_lyrics_updated.gz"))
 data.table::fwrite(DF_paragraphs_updated, paste0(destination_folder, "DF_paragraphs_updated.gz"))
+
+fs::dir_copy("outputs/lyrics_processed/",  paste0(destination_folder, "lyrics_processed/"))
+fs::dir_copy("outputs/zips/",  paste0(destination_folder, "zips/"))
+fs::dir_copy("outputs/DF/",  paste0(destination_folder, "DF/"))
 # DF_lyrics_paragraphs_HITS, DF_lyrics_updated, DF_paragraphs_updated
 # data.table::fwrite()
 
